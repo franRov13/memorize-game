2533633c67d575dd008ffc1df900961185dfa2f8
franRov13 <francisco.a.rovira1@gmail.com>
2025-07-05 17:32:55 -0400

    Added flipping feature for cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -3,10 +3,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
             CardView()
             CardView()
-            CardView()
+            CardView(isFaceUp: false)
         }
         .foregroundColor(.orange)
         .padding()
@@ -16,24 +16,28 @@ struct ContentView: View {
 
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    
+    @State var isFaceUp = true
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻")
                     .font(.largeTitle)
             }else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle() // changes the bool value to it's contrary (e.g if it's true, it changes to false)
         }
     }
 }
 
-//#Preview {
-//    ContentView()
-//}
+#Preview {
+    ContentView()
+}
